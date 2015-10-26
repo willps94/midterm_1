3c058f83012972fed4b08a52837dae8ec8f3a7d0
Will <wtprattstephen@quinnipiac.edu>
2015-10-26 22:52:27 +0000

    Add contact me button

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+   name = models.CharField(max_length=300)
+   email = models.CharField(max_length=300)
+   message = models.TextField(null=True, blank=True)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 class Home(TemplateView):
     template_name = "home.html"
 
 # Create your views here.
+class QuestionCreateView(CreateView):
+    model = Question
+    template_name = "question/question_form.html"
+    fields = ['name', 'email', 'message']
+    success_url = reverse_lazy('home')

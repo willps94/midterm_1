9899bdac590acbca4bb94ac2414c64a351202d71
Will <wtprattstephen@quinnipiac.edu>
2015-10-26 23:08:16 +0000

    Add contact admin page

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ class Question(models.Model):
    name = models.CharField(max_length=300)
    email = models.CharField(max_length=300)
    message = models.TextField(null=True, blank=True)
+
+   def __unicode__(self):
+     return self.name
